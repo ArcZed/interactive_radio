e28fc780e59f2d84c410ce586a82bea39a1aff32
Khoa Tran <92013921+ArcZed@users.noreply.github.com>
2024-11-16 14:42:08 +0700

    add more lofi

--- src/Song.js
+++ src/Song.js
@@ -1,22 +1,28 @@
-import lofi from './music/lofi.mp3';
-import ambient from "./music/ambient.mp3"
-import nature from "./music/nature.mp3"
+
+import lofi1 from './music/lofi/lofi1.mp3';
+import lofi2 from './music/lofi/lofi2.mp3';
+import lofi3 from './music/lofi/lofi3.mp3';
+import lofi4 from './music/lofi/lofi4.mp3';
+import lofi5 from './music/lofi/lofi5.mp3';
+import lofi6 from './music/lofi/lofi6.mp3';
+
+import ambient from "./music/ambient/ambient.mp3"
+import nature from "./music/nature/nature.mp3"
 
 export const playMusic = () => {
 
     const bar = document.querySelector(".bar");
     const meter = document.querySelector(".meter");
-    const audio = document.querySelector("audio")
-
-    console.log(typeof(lofi))
-    
+    const audio = document.querySelector("audio")    
 
     let playlist = [];
+    let lofi = [lofi1, lofi2, lofi3, lofi4, lofi5, lofi6];
     let genreList = [lofi, ambient, nature];
+
+    var prevSong = [];
 
     //get meter pos
     let meterPos = parseInt(window.getComputedStyle(meter).getPropertyValue("left"));
-    console.log(meterPos);
 
     if (meterPos > 0 && meterPos < bar.scrollWidth/3) {
         playlist.push(genreList[0]);
@@ -26,17 +32,25 @@ export const playMusic = () => {
     }
     else {playlist.push(genreList[2]);}
     
-    playlist.forEach((song, i) => {
+    const randomSong = (list) => {
+        let currSong = Math.floor(Math.random() * list[0].length);
+        
+        
+        
+        prevSong.push(currSong);
+        console.log(prevSong);
+        return currSong
+    }
 
-        audio.pause();
-        audio.src = `${playlist[i]}`;
-        audio.play();
-        console.log(audio)
-    });
-    setInterval(() => {
-        console.log(audio.volume);
-    }, 500);
+    let currSong = randomSong(playlist)
+
+    audio.pause();
+    audio.src = `${playlist[0][currSong]}`;
+    audio.play();
+  
     audio.addEventListener("ended", () => {
+        audio.pause();
+        audio.src = `${playlist[0][currSong]}`;
         audio.play();
-    })
+    });
 }
