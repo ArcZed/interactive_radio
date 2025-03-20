ada32bb038d924edec233d642fa3c653769a29f3
Khoa Tran <92013921+ArcZed@users.noreply.github.com>
2025-03-20 18:24:00 +0700

    gradient intro background

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import styles from "./css/styles.css"
+import background_styles from  './css/background.css'
 import screen_styles from "./css/Screen.css"
 import intro_styles from './css/intro.css';
-import background_styles from  './css/background.css'
+
 import { createPage } from "./js/introPage";
 import { createScreen } from "./js/Screen";
 
--- src/js/introPage.js
+++ src/js/introPage.js
@@ -1,30 +1,58 @@
-
-
 const content = document.querySelector("#content");
+const container = document.querySelector(".container")
 
 export const createPage = () => {
    
-    const introPage =document.createElement("div");
+    const introPage = document.createElement("div");
+    const dialog = document.createElement("dialog")
     const title = document.createElement("h1");
     const description = document.createElement("h3");
     const click = document.createElement("h3");
 
-    introPage.className = "introPage"
+    introPage.className = "introPage";
 
     title.textContent = "RAD.IO";
     description.textContent = "Choose a playlist for your studying and relaxing";
     click.textContent = "Click anywhere to start";
 
     content.appendChild(introPage)
-    introPage.appendChild(title);
-    introPage.appendChild(description);
-    introPage.appendChild(click);
+    introPage.appendChild(dialog)
+    dialog.appendChild(title);
+    dialog.appendChild(description);
+    dialog.appendChild(click);
+
+    const blob1 = document.createElement("div");
+    const blob2 = document.createElement("div");
+    const blob3 = document.createElement("div");
+    const blob4 = document.createElement("div");
+    const blobContainer = document.createElement("div");
+
+    introPage.appendChild(blobContainer);
+    blobContainer.appendChild(blob1);
+    blobContainer.appendChild(blob2);
+    blobContainer.appendChild(blob3);
+    blobContainer.appendChild(blob4);
+
+    blobContainer.className = "blobContainer";
+    blob1.className = "blob1";
+    blob2.className = "blob2";
+    blob3.className = "blob3";
+    blob4.className = "blob4";
+
+    container.style.opacity = `${0}%`
+
+    console.log(container)
 
     introPage.addEventListener("click", () => {
         introPage.classList.add("fadeOut");
-        setTimeout(() => {
-            content.removeChild(introPage);
-        }, 500);
+        container.classList.add("fadeIn");
+        container.style.opacity = `${100}%`
+
+        introPage.addEventListener("animationend", () => {
+            setTimeout(() => {
+                content.removeChild(introPage);
+            }, 2000);
+        });
     });
 
     return {introPage}
